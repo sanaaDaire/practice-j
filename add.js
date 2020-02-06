280ed9e3f92aa92071211caba557c35c87f37861
sanaaDaire <sanaa.daire@icloud.com>
2020-02-06 11:49:53 +0200

    javascript-task

---
+++ add.js
@@ -0,0 +1,13 @@
+alert ("GOOD MORNING");
+confirm ("Are you Reader");
+prompt (" Whats your name ?");
+var name = prompt (" whats your name ?");
+var name = prompt ("Whats your age ?");
+if (age > 20) {
+alert (" Your Age is accept");
+} 
+else 
+{ alert (" Your age is young , SORRY!")
+
+}
+
